8150ff4935d986d9ae17bf1e6bd27e75bb005082
Bahaa Mekkawy <bahaa.elmekkawy@gmail.com>
2025-06-24 17:49:41 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Q1
+
+            //int[] numbers = { 5, 1, 4, 2, 8 };
+            //BubbleSort<int>.BubbleSortMethod(ref numbers);
+            //foreach (int i in numbers)
+            //{
+            //    Console.WriteLine(i); 
+
+            //}
+            #endregion
+            #region Q2
+            //Range <double>  range = new Range<double>(30, 50);
+            //Console.WriteLine(range.IsInRange(49));
+            //Console.WriteLine(range.IsInRange(51)); 
+
+            #endregion
+
+
+        }
+    }
+}
---
+++ Range.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    internal class Range<T> where T : IComparable<T>, ISubtractionOperators<T, T, T>
+    {
+
+
+        public T minValue { get; set; }
+        public T maxValue { get; set; }
+
+
+
+        public Range(T min, T max)
+        {
+            this.minValue = min;
+            this.maxValue = max;
+        }
+        public bool IsInRange(T value)
+        {
+
+            if (value.CompareTo(minValue) == 1 && value.CompareTo(maxValue) == -1)
+            {
+                return true;
+
+            }
+            return false;
+
+        }
+        public T Length()
+        {
+
+            return this.maxValue - this.minValue;
+        }
+    }
+
+}
+}
